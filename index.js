51e407df6559314591f77e61d98f41bd23d0f6cc
WhiteLotux <129133826+WhiteLotux@users.noreply.github.com>
2023-10-19 18:13:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send(`Omar Pimentel <br>
+            C.I: V.- 28.050.922 <br>
+            Programacion III Seccion "3"`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Servidor ejecutado exitosamente en el Puerto: ${port}`);
+});
